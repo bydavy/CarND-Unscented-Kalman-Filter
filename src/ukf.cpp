8eaf1869e0f7b8c2d8956590132469abaafc785d
Davy Leggieri <davy.leggieri@gmail.com>
2017-04-17 15:50:51 +0700

    Tweaked the process noise to more realistic values

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -28,10 +28,10 @@ UKF::UKF() {
   P_ = MatrixXd(n_x_, n_x_);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 0.45; // We are tracking a bicycle
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.45; // We are tracking a bicycle
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
